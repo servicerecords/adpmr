792b98674af2fc8b073de19a1affbdc7dd76ac9e
CodeSureUK <55851467+CodeSureUK@users.noreply.github.com>
2021-10-28 14:07:25 +0100

    Update death-in-service.blade.php

--- src/resources/views/death-in-service.blade.php
+++ src/resources/views/death-in-service.blade.php
@@ -3,8 +3,8 @@
 
 @section('content')
     <p class="govuk-body">This RAF application process will not tell you if a service record is held. (
-wait: remote command exited without exit status or exit signaly-military-service-records/apply-for-someone-elses-records">Apply by post for Army, Navy & Home Guard records</a>
-FAILED  )</p>
+      <a class="govuk-link" href="https://www.gov.uk/get-copy-military-service-records/apply-for-someone-elses-records">Apply by post for Army, Navy &amp; Home Guard records</a>
+    )</p>
 
     <form method="post" action="{{ route('death-in-service.save') }}" novalidate>
         <x-error-summary :errors="$errors"></x-error-summary>
