1900ad91940c71314d4f8da8b90ebbbc3dd7221d
CodeSureUK <55851467+CodeSureUK@users.noreply.github.com>
2021-11-19 12:29:25 +0000

    Update ConfirmationController.php

--- src/app/Http/Controllers/ConfirmationController.php
+++ src/app/Http/Controllers/ConfirmationController.php
@@ -19,7 +19,7 @@ class ConfirmationController extends Controller
         $application = Application::getInstance();
 
         if($payment !== true) {
-            return view('check-answers');
+            return redirect()->route('check-answers');
         }
 
         if(session('application-reference', false)) {
